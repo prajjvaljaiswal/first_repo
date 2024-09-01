39b741accbeb48ad61ca57aa2b315f2b9f3ed12f
prajjvaljaiswal <prajjaiswal158@gmail.com>
2024-09-01 18:06:47 +0530

    -🚀 This is my first commit

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+const Header = () => {
+    return(
+        <div>
+            <h1>Title</h1>
+            <div>
+                <ul>
+                    <li>about</li>
+                    <li>contact</li>
+                    <li>cart</li>
+                </ul>
+            </div>
+            <button>Sign-In</button>
+        </div>
+    )
+}
+
+export default Header;
